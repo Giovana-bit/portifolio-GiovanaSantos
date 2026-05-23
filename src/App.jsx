81be612093422dc08d84e73c647a48af0b024e2a
Giovana <giovanadossantos107@gmail.com>
2026-05-23 20:53:11 -0300

    bugs fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Toaster } from "./components/ui/toaster";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import PageNotFound from './libs/PageNotFound';
-import Home from './pages/Home';
+import PageNotFound from "./libs/PageNotFound";
+import Home from "./pages/Home";
 
 function App() {
   return (
